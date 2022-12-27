503bb9fbc53ff7067638718e64059bfbdc2d7a84
Ailsya Putri <ailsyaputri9597@gmail.com>
2022-12-27 15:01:16 +0700

    Komentar

---
+++ CRUDMahasiswaSederhana/src/main/java/com/mycompany/crudmahasiswasederhana/CRUDMahasiswaSederhana.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.crudmahasiswasederhana;
+
+import com.mycompany.crudmahasiswasederhana.view.MahasiswaFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author ASUS
+ * Ailsya Putri / 21103022
+ */
+
+public class CRUDMahasiswaSederhana {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                MahasiswaFrame form = new MahasiswaFrame();
+                form.setLocationRelativeTo(null);
+                form.setVisible(true);
+            }
+        });
+    }
+}
